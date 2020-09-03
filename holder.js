f44cb5b492723cd3eabd913e5a64f106033b1dd3
SupriyaRai15 <69969489+SupriyaRai15@users.noreply.github.com>
2020-09-03 22:01:26 +0530

    Add files via upload

---
+++ holder.js
@@ -0,0 +1,24 @@
+class HOLDER {
+    constructor(x, y, width, height) 
+    {
+      var options = 
+      {
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+       
+    }
+    display(){
+      var pos =this.body.position;
+      var ang =this.body.angle;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x,pos.y, this.width, this.height);
+      
+    }
+  };
+  
